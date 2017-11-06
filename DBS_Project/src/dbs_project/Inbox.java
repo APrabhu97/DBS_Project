09a9afc7ec69192761cd0a2ada12dd133d47a3c6
Anish <anishprabhu1997@gmail.com>
2017-11-06 13:21:41 +0530

    anish:inbox implemented

--- DBS_Project/src/dbs_project/Inbox.java
+++ DBS_Project/src/dbs_project/Inbox.java
@@ -16,6 +16,7 @@ import java.util.Vector;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -26,89 +27,125 @@ public class Inbox extends javax.swing.JFrame {
     /**
      * Creates new form Inbox
      */
-    public Inbox(){}
-    public Inbox(String TOID) {
-        ArrayList columnNames = new ArrayList();
-        ArrayList data = new ArrayList();
+    public Inbox(){
         initComponents();
-        try (Connection conn = Conn.connect();
-                    PreparedStatement pst =   conn.prepareStatement("Select * from messages where to_ID = '"
-                            +TOID+"'");
-                    ResultSet rs = pst.executeQuery();)
-        {
-            ResultSetMetaData md = rs.getMetaData();
-            int columns = md.getColumnCount();
+    DefaultTableModel model = new DefaultTableModel(new String[]{"FROM", "SUBJECT", "MESSAGE"}, 0);
+    try{
+     Connection conn = Conn.connect();
+        PreparedStatement pst =   conn.prepareStatement("SELECT * FROM messages where to_id='photo';");
+    ResultSet rs = pst.executeQuery();
+    while(rs.next())
+{
+    String d = rs.getString("from_id");
+    String e = rs.getString("subject");
+    String f = rs.getString("message");
+    model.addRow(new Object[]{d, e, f});
+    jTable1.setModel(model);
+}
+}catch(Exception e){
+        System.out.println(e);
+}
+    }
 
-            //  Get column names
-            for (int i = 1; i <= columns; i++)
-            {
-                columnNames.add( md.getColumnName(i) );
-            }
-
-            //  Get row data
-            while (rs.next())
-            {
-                ArrayList row = new ArrayList(columns);
-
-                for (int i = 1; i <= columns; i++)
-                {
-                    row.add( rs.getObject(i) );
-                }
-
-                data.add( row );
-            }
-        }
-        catch (SQLException e)
-        {
-            System.out.println( e.getMessage() );
-        }
-
-     
-        Vector columnNamesVector = new Vector();
-        Vector dataVector = new Vector();
-
-        for (int i = 0; i < data.size(); i++)
-        {
-            ArrayList subArray = (ArrayList)data.get(i);
-            Vector subVector = new Vector();
-            for (int j = 0; j < subArray.size(); j++)
-            {
-                subVector.add(subArray.get(j));
-            }
-            dataVector.add(subVector);
-        }
-
-        for (int i = 0; i < columnNames.size(); i++ )
-            columnNamesVector.add(columnNames.get(i));
-
-        //  Create table with database data    
-        JTable table = new JTable(dataVector, columnNamesVector)
-        {
-            public Class getColumnClass(int column)
-            {
-                for (int row = 0; row < getRowCount(); row++)
-                {
-                    Object o = getValueAt(row, column);
-
-                    if (o != null)
-                    {
-                        return o.getClass();
-                    }
-                }
-
-                return Object.class;
-            }
-        };
-
-        JScrollPane scrollPane = new JScrollPane( table );
-        getContentPane().add( scrollPane );
-
-        JPanel buttonPanel = new JPanel();
-        getContentPane().add( buttonPanel, BorderLayout.SOUTH );
-        Inbox frame = new Inbox();
-        frame.setDefaultCloseOperation( EXIT_ON_CLOSE );
-        frame.pack();
-        frame.setVisible(true);
+    public Inbox(String TOID) {
+        initComponents();
+    DefaultTableModel model = new DefaultTableModel(new String[]{"FROM", "SUBJECT", "MESSAGE"}, 0);
+    try{
+     Connection conn = Conn.connect();
+        PreparedStatement pst =   conn.prepareStatement("SELECT * FROM messages where to_id='"+TOID+"';");
+    ResultSet rs = pst.executeQuery();
+    while(rs.next())
+{
+    String d = rs.getString("from_id");
+    String e = rs.getString("subject");
+    String f = rs.getString("message");
+    model.addRow(new Object[]{d, e, f});
+    jTable1.setModel(model);
+}
+}catch(Exception e){
+        System.out.println(e);
+}
+//        ArrayList columnNames = new ArrayList();
+//        ArrayList data = new ArrayList();
+//        initComponents();
+//        try (Connection conn = Conn.connect();
+//                    PreparedStatement pst =   conn.prepareStatement("Select * from messages where to_ID = '"
+//                            +TOID+"'");
+//                    ResultSet rs = pst.executeQuery();)
+//        {
+//            ResultSetMetaData md = rs.getMetaData();
+//            int columns = md.getColumnCount();
+//
+//            //  Get column names
+//            for (int i = 1; i <= columns; i++)
+//            {
+//                columnNames.add( md.getColumnName(i) );
+//            }
+//
+//            //  Get row data
+//            while (rs.next())
+//            {
+//                ArrayList row = new ArrayList(columns);
+//
+//                for (int i = 1; i <= columns; i++)
+//                {
+//                    row.add( rs.getObject(i) );
+//                }
+//
+//                data.add( row );
+//            }
+//        }
+//        catch (SQLException e)
+//        {
+//            System.out.println( e.getMessage() );
+//        }
+//
+//     
+//        Vector columnNamesVector = new Vector();
+//        Vector dataVector = new Vector();
+//
+//        for (int i = 0; i < data.size(); i++)
+//        {
+//            ArrayList subArray = (ArrayList)data.get(i);
+//            Vector subVector = new Vector();
+//            for (int j = 0; j < subArray.size(); j++)
+//            {
+//                subVector.add(subArray.get(j));
+//            }
+//            dataVector.add(subVector);
+//        }
+//
+//        for (int i = 0; i < columnNames.size(); i++ )
+//            columnNamesVector.add(columnNames.get(i));
+//
+//        //  Create table with database data    
+//        JTable table = new JTable(dataVector, columnNamesVector)
+//        {
+//            public Class getColumnClass(int column)
+//            {
+//                for (int row = 0; row < getRowCount(); row++)
+//                {
+//                    Object o = getValueAt(row, column);
+//
+//                    if (o != null)
+//                    {
+//                        return o.getClass();
+//                    }
+//                }
+//
+//                return Object.class;
+//            }
+//        };
+//
+//        JScrollPane scrollPane = new JScrollPane( table );
+//        getContentPane().add( scrollPane );
+//
+//        JPanel buttonPanel = new JPanel();
+//        getContentPane().add( buttonPanel, BorderLayout.SOUTH );
+//        Inbox frame = new Inbox();
+//        frame.setDefaultCloseOperation( EXIT_ON_CLOSE );
+//        frame.pack();
+//        frame.setVisible(true);
     }
 
     /**
